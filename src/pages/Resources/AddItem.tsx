b2672ccfbac35141c706529108f7c7a9bbbc59dc
mudassirpk <mudassir.pk.mp@gmail.com>
2024-11-19 16:10:21 +0500

    update: mutation flow

--- src/pages/Resources/AddItem.tsx
+++ src/pages/Resources/AddItem.tsx
@@ -1,12 +1,20 @@
 import AttributeInput from "@/components/resource-item/AttributeInput";
+import { TDropdownSelectItem } from "@/components/resource-item/SearchDropdown";
 import { Button } from "@/components/ui/button";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { FormEvent, useState } from "react";
+import toast from "react-hot-toast";
 import { useParams } from "react-router-dom";
 import { TResource } from "types";
 
 export default function AddItem() {
   const params = useParams();
+
+  const [relations, setRelations] = useState<
+    { name: string; value: TDropdownSelectItem[] }[]
+  >([]);
+
   const { data, isFetching } = useQuery<TResource>({
     queryKey: ["get-resource-details_add_item"],
     async queryFn() {
@@ -19,15 +27,42 @@ export default function AddItem() {
   });
 
   const { mutate } = useMutation({
-    async mutationFn(data: any) {
+    async mutationFn(data: unknown) {
       return (await axios.post("http://localhost:3000/resource/item", data))
         .data;
+    },
+    onSuccess(response) {
+      if (response.success) {
+        toast.success(`New ${data?.name} added successfully`);
+      }
     },
   });
 
   // const {data} = useQuery({
   //   queryKey:'relation_resource_atom',
   // })
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    const data: { [key: string]: string | string[] | FormDataEntryValue } =
+      Object.fromEntries(new FormData(e.currentTarget as HTMLFormElement));
+
+    // append the selected resource for each relation to the formdata
+    for (const relation of relations) {
+      data[relation.name] = relation.value.map((r) => r.value) as string[];
+    }
+
+    const values = [];
+    for (const v of Object.keys(data)) {
+      values.push({
+        name: v,
+        value: data[v],
+      });
+    }
+
+    if (params.resourceId)
+      mutate({ values, resource: parseInt(params.resourceId) });
+  }
 
   return (
     <div className="w-full p-4">
@@ -36,27 +71,17 @@ export default function AddItem() {
       ) : (
         <div className="w-full">
           <h2 className="font-semibold text-xl">Add new {data?.name}</h2>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              const data = Object.fromEntries(new FormData(e.currentTarget));
-              const values = [];
-              for (const v of Object.keys(data)) {
-                values.push({
-                  name: v,
-                  value: data[v],
-                });
-              }
-
-              mutate({ values, resource: parseInt(params.resourceId as any) });
-            }}
-            className="my-4 flex flex-col gap-2"
-          >
-            {data?.attributes.map((attribute,i:number) => {
+          <form onSubmit={handleSubmit} className="my-4 flex flex-col gap-2">
+            {data?.attributes.map((attribute, i: number) => {
               return (
                 <>
-                  <AttributeInput attribute={attribute} />
-                  {i!==data?.attributes.length-1 && <p className="w-full border-b my-2"></p>}
+                  <AttributeInput
+                    setRelations={setRelations}
+                    attribute={attribute}
+                  />
+                  {i !== data?.attributes.length - 1 && (
+                    <p className="w-full border-b my-2"></p>
+                  )}
                 </>
               );
             })}
