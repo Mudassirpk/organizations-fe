2f31c754f67a473932160403d27334b7052f98f2
mudassirpk <mudassir.pk.mp@gmail.com>
2024-11-25 22:11:55 +0500

    fix: add missing organizationId in post data while creating new role

--- src/components/roles/AddRoleModal.tsx
+++ src/components/roles/AddRoleModal.tsx
@@ -7,44 +7,57 @@ import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { queryClient } from "@/lib/query_client.ts";
+import { useAuth } from "@/store/contexts/context";
 
 export default function AddRoleModal() {
   const [open, setOpen] = React.useState(false);
+  const { user } = useAuth();
 
   const { mutate, status } = useMutation({
-    async mutationFn(data: { roleName: string }) {
-      return (await axios.post("http://localhost:3000/role", data)).data
-    }
-    , onSuccess(response) {
+    async mutationFn(data: { roleName: string; organizationId?: number }) {
+      return (await axios.post("http://localhost:3000/role", data)).data;
+    },
+    onSuccess(response) {
       if (response.success) {
-        toast.success(response.message)
-        queryClient.invalidateQueries({ queryKey: ['get-organization-roles'] })
+        toast.success(response.message);
+        queryClient.invalidateQueries({ queryKey: ["get-organization-roles"] });
       } else {
-        toast.error(response.message)
+        toast.error(response.message);
       }
-    }
-  })
+    },
+  });
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
 
     const formData = new FormData(e.currentTarget as HTMLFormElement);
-    mutate({ roleName: formData.get('name') as string })
+    mutate({
+      roleName: formData.get("name") as string,
+      organizationId: user?.user_organization[0].organizationId,
+    });
   }
 
-  return <Modal title={'Add Role'} triggerTitle={"Add Role"} open={open} setOpen={setOpen}>
-    <form onSubmit={handleSubmit} className={'flex flex-col gap-2'}>
-      <div className="space-y-2">
-        <Label htmlFor="name">Name</Label>
-        <Input
-          id="name"
-          name="name"
-          placeholder="John Doe"
-          required
-        />
-      </div>
-      <Button disabled={status === 'pending'}
-        className={'disabled:bg-gray-400 w-full bg-blue-600 hover:bg-blue-500 text-white'}>Add Role</Button>
-    </form>
-  </Modal>
+  return (
+    <Modal
+      title={"Add Role"}
+      triggerTitle={"Add Role"}
+      open={open}
+      setOpen={setOpen}
+    >
+      <form onSubmit={handleSubmit} className={"flex flex-col gap-2"}>
+        <div className="space-y-2">
+          <Label htmlFor="name">Name</Label>
+          <Input id="name" name="name" placeholder="John Doe" required />
+        </div>
+        <Button
+          disabled={status === "pending"}
+          className={
+            "disabled:bg-gray-400 w-full bg-blue-600 hover:bg-blue-500 text-white"
+          }
+        >
+          Add Role
+        </Button>
+      </form>
+    </Modal>
+  );
 }
